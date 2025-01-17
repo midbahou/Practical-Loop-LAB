d5fdd967176ff6b6acb95452e9ccdf5882318ee8
midbahou <midbahou@gmail.com>
2025-01-16 19:29:07 -0500

    Part 3 added

--- practical-Loops.js
+++ practical-Loops.js
@@ -44,7 +44,7 @@ let n = parseInt(prompt(`Enter a number here:`)); // Declaring an arbitrary numb
 let nextPrime = n + 1; // starting at n and incrementing from there.
 while (true) {
     let isPrime = true; // Assuming the number is prime
-
+    
     // Check 
     for (let i = 2; i < nextPrime; i++) {
         if (nextPrime % i === 0) {
@@ -56,6 +56,52 @@ while (true) {
         console.log(`The Next Prime Number after ${n} is ${nextPrime}`);
         break; // To exit the while loop
     }
-
+    
     nextPrime++; // Increment to check the next number
+}
+
+
+
+// ============================== Part 3: Feeling Loopy ===============================================
+console.log("=============== Part 3 ==============");
+
+//*TODO: Your task is to write a script that accomplishes the following:
+    //* Loop through the characters of a given CSV string.
+    //* Store each “cell” of data in a variable.
+    //* When you encounter a comma, move to the next cell.
+    //* When you encounter the “\r\n” sequence, move to the next “row.”
+    //* Log each row of data.
+
+// example of a CVS String without using arrays 
+const cvsString = `Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232`;
+
+// let's create variables to stopre cells and rows data
+let cell = "";
+let row = "";
+
+// Loop into each character in the CVS string
+for (let i = 0; i < cvsString.length; i++) {
+    const char = cvsString[i]; // reference to the current character
+
+    // if the char is "," make a space and if it's \n go to the next row
+    if (char === ",") {
+        // A comma indicates the end of a cell
+        row += cell + " ";  // Add the cell to the row
+        cell = ""; // reset the cell
+        // console.log(row); to print intermediate values of each cell.
+    } else if (char === "\n") {
+        // A newline indicates the end of a row
+        row += cell; // Add the last cell to the row
+        console.log(row); // Log the entire row
+        // reset the cell and the row
+        row = ""; 
+        cell = "";
+    } else {
+        cell += char; // Accumulate characters for the current cell
+    }
+} 
+// Log the last row if there's no trailing newline
+if (cell) {
+    row += cell;
+    console.log(row);
 }
